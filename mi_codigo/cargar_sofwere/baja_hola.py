80307832623a0efd142f6f207ef2430e89edbfde
Jesús Escobar <55922440+Jem-alchemist@users.noreply.github.com>
2019-09-29 17:24:50 +0200

    corregir

--- mi_codigo/cargar_sofwere/baja_hola.py
+++ mi_codigo/cargar_sofwere/baja_hola.py
@@ -3,7 +3,7 @@
 # Cargar lista.py en memoria lo graba como fichero y ejecuta el fichero.
 
 import urequests
-url_base ='https://raw.githubusercontent.com/Jem-alchemist/MicroPython/mi_codigo/cargar_sofwere/master/'
+url_base ='https://raw.githubusercontent.com/Jem-alchemist/MicroPython/master/mi_codigo/cargar_sofwere/'
 r = urequests.get(url_base + 'lista.py')
 f = open('lista.py', 'w')
 f.write(r.text)
--- mi_codigo/cargar_sofwere/carga_repo.py
+++ mi_codigo/cargar_sofwere/carga_repo.py
@@ -3,7 +3,7 @@
 # Cargar lista.py en memoria de bichoo y lo ejecuta.
 
 import urequests
-url_base ='https://raw.githubusercontent.com/Jem-alchemist/MicroPython/mi_codigo/cargar_sofwere/master/'
+url_base ='https://raw.githubusercontent.com/Jem-alchemist/MicroPython/master/mi_codigo/cargar_sofwere/'
 r = urequests.get(url_base + 'lista.py')
 exec (r.text)
 
