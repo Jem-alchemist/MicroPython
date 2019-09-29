d323b86e0a3384080dfb0159bc33500ebc53f53a
Jesús Escobar <55922440+Jem-alchemist@users.noreply.github.com>
2019-09-29 18:42:59 +0200

    mejora

--- mi_codigo/cargar_sofwere/test.py
+++ mi_codigo/cargar_sofwere/test.py
@@ -19,15 +19,15 @@ print("version:" + os.uname().version)
 print("Maquina:" + os.uname().machine)
 print("Frecuencia de relog del procesador:" + str(machine.freq()/1000000) + "MHz")
 gc.collect()
-titulo="Datos de memoria"
+titulo="\nDatos de memoria"
 print(titulo)
-print("\n" + "-" * len(titulo))                                                                
+print("-" * len(titulo))                                                                
 print("Memoria RAM disponible:" + str(gc.mem_free()/1024) + "Kbytes")
 print("Memoria flash:" + str(esp.flash_size ()/1024) + "Kbytes")
 print ("Memoria flash del usuario:" + str(esp.flash_user_start()/1024) + "Kbytes")
-titulo="Listamos los ficheros"
+titulo="\nListamos los ficheros"
 print(titulo)
-print("\n" + "-" * len(titulo))
+print("-" * len(titulo))
 for fichero in os.listdir():
   print(fichero)
 
