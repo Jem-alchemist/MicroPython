8512ff26dd9bbefa8ef6924bb3983b9348671d0b
Jesús Escobar <55922440+Jem-alchemist@users.noreply.github.com>
2019-09-29 18:40:01 +0200

    mejora

--- mi_codigo/cargar_sofwere/test.py
+++ mi_codigo/cargar_sofwere/test.py
@@ -6,18 +6,28 @@ import os
 import sys
 import machine
 import esp
+titulo="Informacion general del microcontrolador"
+print(titulo)
+print("-" * len(titulo) +"\n")
+titulo="Datos del sistema"
+print(titulo)
+print("-" * len(titulo))
 print("Nombre del sistema:" + os.uname().sysname)
 print("Nombre del nodo:" + os.uname().nodename)
 print("Lanzamiento:" + os.uname().release)
 print("version:" + os.uname().version)
 print("Maquina:" + os.uname().machine)
 print("Frecuencia de relog del procesador:" + str(machine.freq()/1000000) + "MHz")
-gc.collect()                                                                
+gc.collect()
+titulo="Datos de memoria"
+print(titulo)
+print("\n" + "-" * len(titulo))                                                                
 print("Memoria RAM disponible:" + str(gc.mem_free()/1024) + "Kbytes")
 print("Memoria flash:" + str(esp.flash_size ()/1024) + "Kbytes")
 print ("Memoria flash del usuario:" + str(esp.flash_user_start()/1024) + "Kbytes")
-print("Listamos los ficheros")
-print("-" * 21)
+titulo="Listamos los ficheros"
+print(titulo)
+print("\n" + "-" * len(titulo))
 for fichero in os.listdir():
   print(fichero)
 
