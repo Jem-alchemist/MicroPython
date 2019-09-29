e6380c97f04b21fa6353d9d5a9bd66a0c1d667e9
Jesús Escobar <55922440+Jem-alchemist@users.noreply.github.com>
2019-09-29 21:01:53 +0200

    Update simple_servidor.py

--- mi_codigo/cargar_sofwere/simple_servidor.py
+++ mi_codigo/cargar_sofwere/simple_servidor.py
@@ -4,6 +4,8 @@
 
 
 import usocket as socket
+from machine import Pin
+pin_out = Pin(2, Pin.OUT)
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
 s.listen(5)
@@ -13,21 +15,21 @@ while True:
     print('Nueva conexion %s' % str(addr))
     request = conn.recv(1024)
     comando = str(request, 'utf-8')
-    if comando[0:5] == "final":
+    if comando[5:10] == "final":
       print("Comando fianl")
       conn.send("Final del proceso" + "\n")
       break
-    elif comando[0:5] == "ledon":
+    elif comando[5:10] == "ledon":
       print("Comando ledon")
       conn.send("Led encendido" + "\n")
       pin_out.off()
-    elif comando[0:5] == "ledof":
+    elif comando[5:10] == "ledof":
       print("Comando ledoff")
       conn.send("Led apagado" + "\n")
       pin_out.on()
     else:
       print("Envio un comando no reconocido")
-      conn.send(comando[5:10] + "Comando no contemplado" + "\n")
+      conn.send("Mando:" + comando[5:10] + " comando no contemplado" + "\n")
     conn.close()
   except:
     print("Error")
