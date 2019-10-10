e340cc0b9c43447f1d7da6873da441d41d03c27d
Jesús Escobar <55922440+Jem-alchemist@users.noreply.github.com>
2019-10-10 20:30:19 +0200

    Update servidor_web.py

--- mi_codigo/cliente-servidor/servidor_web.py
+++ mi_codigo/cliente-servidor/servidor_web.py
@@ -1,6 +1,6 @@
 # Jesus Manuel Escobar Muñoz
-# 29 de Septiembre de 2019
-# Ejemplo de un simple servidor
+# 10 de Octubre de 2019
+# Ejemplo de un servidor web
 
 
 import usocket as socket
@@ -9,13 +9,14 @@ s.bind(('', 80))
 s.listen(5)
 
 while True:
- # try:
+  try:
     conn, addr = s.accept()
     print('Nueva conexion %s' % str(addr))
     request = conn.recv(1024)
     request = str(request)
     print('Content = %s' % request)
     pagina = request.split(' ')[1][1:]
+    if pagina == "parar_bucle": break
     try:
       response = open(pagina).read()
     except:
@@ -25,6 +26,6 @@ while True:
     conn.send('Connection: close\n\n')
     conn.sendall(response)
     conn.close()
-  #except:
-   # print("Error")
+  except:
+    print("Error")
 s.close()
