48d69a77d77fc6658d4fe22a90c187e4dc8b2187
Jesús Escobar <55922440+Jem-alchemist@users.noreply.github.com>
2019-10-10 20:10:09 +0200

    Update servidor_web.py

--- mi_codigo/cliente-servidor/servidor_web.py
+++ mi_codigo/cliente-servidor/servidor_web.py
@@ -8,18 +8,6 @@ s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind(('', 80))
 s.listen(5)
 
-web_primera = """<html><head> <title>Servidor Jem</title> <meta name="viewport" content="width=device-width, initial-scale=1">
-  <body> <p><a href="/segunda"><img src="https://i0.wp.com/radiotgw.gob.gt/wp-content/uploads/2018/11/Elefante.jpg?resize=850%2C425&amp;ssl=1" alt="elefante" width="850" height="425" /></a></p>
-<p>&nbsp;</p></body></html>"""
-  
-web_segunda = """<html><head> <title>Servidor Jem</title> <meta name="viewport" content="width=device-width, initial-scale=1">
-  <body> <p><a href="/primera"><img src="https://static.abc.es/media/sociedad/2016/09/25/elefante-africano-kz7G--620x349@abc.jpg" alt="elefante" width="850" height="425" /></a></p>
-<p>&nbsp;</p></body></html>"""
-
-web_entrada ="""<html><head> <title>Servidor Jem</title> <meta name="viewport" content="width=device-width, initial-scale=1">
-  <body> <p><a href="/primera"><img style="display: block; margin-left: auto; margin-right: auto;" src="https://static.abc.es/media/sociedad/2016/09/25/elefante-africano-kz7G--620x349@abc.jpg" alt="elefante" width="850" height="425" /></a></p>
-<p>&nbsp;</p></body></html>"""
-
 while True:
  # try:
     conn, addr = s.accept()
@@ -27,18 +15,11 @@ while True:
     request = conn.recv(1024)
     request = str(request)
     print('Content = %s' % request)
-    print('s='+request.split(' ')[1])
-    primera = request.find('/primera')
-    segunda = request.find('/segunda')
-    if primera == 6:
-      print('primera')
-      response = web_primera
-    elif segunda == 6:
-      print('segunda')
-      response = web_segunda
-    else:
-      print('primera')
-      response = web_entrada
+    pagina = request.split(' ')[1][1:]
+    try:
+      response = open(pagina).read()
+    except:
+      response = open('index.html').read()
     conn.send('HTTP/1.1 200 OK\n')
     conn.send('Content-Type: text/html\n')
     conn.send('Connection: close\n\n')
