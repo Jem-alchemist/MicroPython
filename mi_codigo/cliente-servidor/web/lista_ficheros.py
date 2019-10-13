b0e5f70e662b9ba54fee0d4ba6d3cf9b6c378708
Jesús Escobar <55922440+Jem-alchemist@users.noreply.github.com>
2019-10-13 14:18:40 +0200

    Create lista_ficheros.py

---
+++ mi_codigo/cliente-servidor/web/lista_ficheros.py
@@ -0,0 +1,8 @@
+def resultado(datos_entrada):
+  import os
+  ficheros = os.listdir()
+  html = '<html><head><title>Servidor Jem</title><meta name="viewport" content="width=device-width, initial-scale=1"><body>'
+  for fichero in ficheros: 
+    html += '<p><a href="' + fichero + '" target="_blank" rel="noopener">' + fichero + '/a></p>'
+  html += '</body></html>'
+  return html
